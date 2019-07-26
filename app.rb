125fcac5a65d1cadc0374a07e93be0e3d81d3e01
Irina Kagan <irinagamb@gmail.com>
2019-07-26 18:03:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,15 +5,5 @@ class App < Sinatra::Base
     "Hello, World!"
   end
 
-   get '/name' do
-     "My name is "
-   end
-   get '/hometown' do
-     "My name is "
-   end get '/name' do
-      "My hometown is "
-    end
-    get '/favorite-song' do
-      "My favorite song is "
-    end
+  
   end 
